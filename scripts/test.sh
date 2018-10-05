438bcb5b4aca2dc2adfa98288124d574599f6cc0
arima <arijun0307@gmail.com>
2018-10-05 19:38:25 +0900

    add scripts

---
+++ scripts/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source /opt/ros/kinetic/setup.bash
+source /home/amsl/ros_catkin_ws/devel/setup.bash
+source ~/.bashrc
+
+gnome-terminal -e "/opt/ros/kinetic/bin/roscore" --geometry=50x12+20+20 &
+sleep 3s
+
+####rviz####
+gnome-terminal -e "/opt/ros/kinetic/bin/rosrun rviz rviz -d /home/amsl/ros_catkin_ws/src/INFANT/config/tsukuba2018.rviz" --geometry=50x12+20+250 
