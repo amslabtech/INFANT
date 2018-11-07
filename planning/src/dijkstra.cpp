0c1e8bc4920baeeb6dfd9a21eb0f517c9a650175
arima <arijun0307@gmail.com>
2018-11-07 12:51:15 +0900

    update

--- planning/src/dijkstra.cpp
+++ planning/src/dijkstra.cpp
@@ -103,11 +103,11 @@ void GlobalPathCreator()
     FILE *fp1;
     FILE *fp2;
     // fp1=fopen("./tsukuba.csv","r"); //tsukuba
-    //fp1=fopen("/home/amsl/ros_catkin_ws/src/INFANT/planning/src/ikuta.csv","r"); //ikuta
-    fp1=fopen("/home/amsl/ros_catkin_ws/src/mapping/latlng2xy/xy/xy_new.csv","r"); //ikuta
+    fp1=fopen("/home/amsl/ros_catkin_ws/src/INFANT/planning/src/ikuta.csv","r"); //ikuta
+    //fp1=fopen("/home/amsl/ros_catkin_ws/src/mapping/latlng2xy/xy/xy_new.csv","r"); //ikuta
     //fp1=fopen("/home/amsl/ros_catkin_ws/src/INFANT/planning/src/xy_new.csv","r"); //ikuta
-    fp2=fopen("/home/amsl/ros_catkin_ws/src/INFANT/planning/src/dijkstra_new.path","r");
-    //fp2=fopen("/home/amsl/ros_catkin_ws/src/INFANT/planning/src/dijkstra.path","r");
+    //fp2=fopen("/home/amsl/ros_catkin_ws/src/INFANT/planning/src/dijkstra_new.path","r");
+    fp2=fopen("/home/amsl/ros_catkin_ws/src/INFANT/planning/src/dijkstra.path","r");
     char VorE[300][10];
     float NE_posi[300][4];
     float Dijkstra[300][3];
@@ -541,7 +541,7 @@ void GlobalPathCreator()
             yaw_cnt++;
         }
         Global_path[0][1]=100;
-        // cout<<"yaw_flag="<<yaw_flag<<endl;
+        cout<<"yaw_flag="<<yaw_flag<<endl;
         if(!yaw_flag){
             if(Global_path[goal_cnt][0]==estimate_start){
                 cout<<"aaaaaaaaaaa"<<endl;
