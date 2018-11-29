6409d790ffbbed8826a32855719c25be4b2a7538
amsl-bald <amslab-git@meiji.ac.jp>
2018-11-29 18:25:59 +1100

    updated

--- recognition/extract_pc_odom_imu.sh
+++ recognition/extract_pc_odom_imu.sh
@@ -7,9 +7,9 @@ gnome-terminal -e "/opt/ros/kinetic/bin/roslaunch infant_recognition recognition
 gnome-terminal -e "/opt/ros/kinetic/bin/roslaunch velodyne_pointcloud 32e_points.launch"
 gnome-terminal -e "/opt/ros/kinetic/bin/rosrun velodyne_height_map rm_ground_node_minmax"
 sleep 5s
-# gnome-terminal -e "rosbag play /media/amsl/D8A80472A8045184/bagfiles/2018-10-13-13-23-54.bag"
+gnome-terminal -e "/opt/ros/kinetic/bin/rosbag play --clock /home/amsl/bagfiles/2018-10-13-13-23-54.bag"
 # gnome-terminal -e "/opt/ros/kinetic/bin/rosbag record /camera/rgb/resized_image/compressed /imu/data /tinypower/odom /velodyne_packets /zed_grasspoints /zed_roadpoints /deeplab/image /camera/depth/down_depth_image -O /home/amsl/Desktop/extract_pc_odom_imu.bag"
-gnome-terminal -e "/opt/ros/kinetic/bin/rosbag record /camera/rgb/resized_image/compressed /imu/data /tinypower/odom /velodyne_packets /zed_grasspoints /zed_roadpoints -O /home/amsl/Desktop/extract_pc_odom_imu.bag"
+# gnome-terminal -e "/opt/ros/kinetic/bin/rosbag record /camera/rgb/resized_image/compressed /imu/data /tinypower/odom /velodyne_packets /zed_grasspoints /zed_roadpoints -O /home/amsl/Desktop/extract_pc_odom_imu.bag"
 gnome-terminal -e "/opt/ros/kinetic/bin/rviz -d /home/amsl/.rviz/localmap_tsuku18.rviz"
 gnome-terminal -e "/opt/ros/kinetic/bin/rviz -d /home/amsl/.rviz/localmap_lidar_tsuku18.rviz"
 gnome-terminal -e "/opt/ros/kinetic/bin/rviz -d /home/amsl/.rviz/localmap_zed_tsuku18.rviz"
