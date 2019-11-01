07fa3cf326a4380d69d4a9cdc315dc80f435b1f0
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-11-01 11:44:01 +0900

    clear

--- scripts/ssh_vanilla/down_rate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ssh -t -t vanilla -X<<EOF
-export ROS_MASTER_URI="http://192.168.0.145:11311"
-sleep 2s
-
-/opt/ros/kinetic/bin/rosrun depth2point down_rate &
-sleep 5s
-
--- scripts/ssh_vanilla/zed_start_vanilla.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ssh -t -t vanilla <<EOF
-export ROS_MASTER_URI="http://192.168.0.145:11311"
-sleep 2s
-/opt/ros/kinetic/bin/roslaunch zed_wrapper zed.launch &
-sleep 5s
-EOF
