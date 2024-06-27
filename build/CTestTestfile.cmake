772d14cb33a73c92e7d213965100316445378c40
YoshikiMaekawa2000 <yoshiki.maekawa@icloud.com>
2024-06-27 14:04:47 +0900

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/amsl/catkin_ws/src/INFANT
+# Build directory: /home/amsl/catkin_ws/src/INFANT/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest
+# Build directory: /home/amsl/catkin_ws/src/INFANT/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
